5ecd5f4c4307f813c4e3ac04cba7e2405fc44bcf
qianyi-sun <sqy201x@hotmail.com>
2023-04-21 15:22:09 -0400

    add more scenarios

---
+++ scenarios/argoverse/platoon/ff239c9d-e4ff-4acc-bad5-bd55648c212e_agents_1/scenario.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from smarts.sstudio import gen_scenario
+from smarts.sstudio import types as t
+from smarts.core.colors import Colors
+
+# scenario_path is a directory with the following structure:
+# /path/to/dataset/{scenario_id}
+# ├── log_map_archive_{scenario_id}.json
+# └── scenario_{scenario_id}.parquet
+
+PATH = "argoverse/data"
+scenario_id = "ff239c9d-e4ff-4acc-bad5-bd55648c212e"  # e.g. "0000b6ab-e100-4f6b-aee8-b520b57c0530"
+scenario_path = (
+    Path(__file__).resolve().parents[4] / PATH / scenario_id
+)  # e.g. Path("/home/user/argoverse/train/") / scenario_id
+
+traffic_histories = [
+    t.TrafficHistoryDataset(
+        name=f"argoverse_{scenario_id}",
+        source_type="Argoverse",
+        input_path=scenario_path,
+    )
+]
+ego_mission = [t.EndlessMission(begin=("road-202833190-202832889-202833142", 2, 33.9))]
+
+leader_id = 46408
+
+gen_scenario(
+    t.Scenario(
+        ego_missions=ego_mission,
+        map_spec=t.MapSpec(source=f"{scenario_path}", lanepoint_spacing=1.0),
+        traffic_histories=traffic_histories,
+        scenario_metadata=t.ScenarioMetadata(leader_id, Colors.Blue),
+    ),
+    output_dir=Path(__file__).parent,
+)
---
+++ scenarios/argoverse/platoon/ffd10ec2-715b-48af-a89d-b11f79927f63_agents_1/scenario.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+from smarts.sstudio import gen_scenario
+from smarts.sstudio import types as t
+from smarts.core.colors import Colors
+
+# scenario_path is a directory with the following structure:
+# /path/to/dataset/{scenario_id}
+# ├── log_map_archive_{scenario_id}.json
+# └── scenario_{scenario_id}.parquet
+
+PATH = "argoverse/data"
+scenario_id = "ffd10ec2-715b-48af-a89d-b11f79927f63"  # e.g. "0000b6ab-e100-4f6b-aee8-b520b57c0530"
+scenario_path = (
+    Path(__file__).resolve().parents[4] / PATH / scenario_id
+)  # e.g. Path("/home/user/argoverse/train/") / scenario_id
+
+traffic_histories = [
+    t.TrafficHistoryDataset(
+        name=f"argoverse_{scenario_id}",
+        source_type="Argoverse",
+        input_path=scenario_path,
+    )
+]
+ego_mission = [
+    t.EndlessMission(
+        begin=("road-190016713-190015427-190016290-190015703", 1, 0.7), start_time=1
+    )
+]
+
+leader_id = 56304
+gen_scenario(
+    t.Scenario(
+        ego_missions=ego_mission,
+        map_spec=t.MapSpec(source=f"{scenario_path}", lanepoint_spacing=1.0),
+        traffic_histories=traffic_histories,
+        scenario_metadata=t.ScenarioMetadata(leader_id, Colors.Blue),
+    ),
+    output_dir=Path(__file__).parent,
+)
